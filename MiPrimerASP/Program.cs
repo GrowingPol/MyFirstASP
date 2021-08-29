52c432fa4c6faa1cc7b699a115ba89ebc1c3c5e9
GrowingPol <growingpol@gmail.com>
2021-08-29 17:46:27 -0600

    Edit Text In Privacy.cshtml

--- MiPrimerASP/Program.cs
+++ MiPrimerASP/Program.cs
@@ -13,7 +13,7 @@ namespace MiPrimerASP
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run(); 
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
